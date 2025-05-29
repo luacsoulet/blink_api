ee8796c39a2f1c3129dbb123924f87364b4ae693
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-05-29 17:40:58 +0200

    Add /users/:id route with schema validation and controller binding
    - Registered the GET /users/:id route using Fastify.
    - Linked the route to the `getUsersById` controller and applied the `usersByIdSchema` for response validation and Swagger documentation.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from "fastify";
-import { getUsers } from "../controllers/userControllers";
-import { usersSchema } from "../dtos/userDtos";
+import { getUserById, getUsers } from "../controllers/userControllers";
+import { userByIdSchema, usersSchema } from "../dtos/userDtos";
 
 export default async function userRoutes(fastify: FastifyInstance) {
     fastify.get('/users', {
         schema: usersSchema
     }, getUsers);
+    fastify.get('/users/:id', {
+        schema: userByIdSchema
+    }, getUserById);
 }
