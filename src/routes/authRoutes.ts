688dc574c70e55c66a1a104932038c369183faf8
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-05-29 15:29:45 +0200

    Import register schema from dtos & implement register routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from "fastify";
-import { loginUser } from "../controllers/authControllers";
-import { loginSchema } from "../dtos/authDtos";
+import { loginUser, registerUser } from "../controllers/authControllers";
+import { loginSchema, registerSchema } from "../dtos/authDtos";
 
 export default async function authRoutes(fastify: FastifyInstance) {
     fastify.post('/login', {
         schema: loginSchema
     }, loginUser);
+    fastify.post('/register', {
+        schema: registerSchema
+    }, registerUser);
 }
